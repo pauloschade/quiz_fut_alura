084bf11e0b0aa696706d7cae0d5e467bb54f87df
Paulo Chade <paulosc4@al.insper.edu.br>
2021-01-28 17:23:06 -0300

    aaaa

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// eslint-disable-next-line no-unused-vars
 import styled from 'styled-components';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -8,8 +8,11 @@ import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -18,36 +21,23 @@ import GitHubCorner from '../src/components/GitHubCorner';
 //   background-position: center;
 // `;
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
-
 export default function Home() {
   const router = useRouter();
   const [name, setName] = React.useState('');
 
-  const hamndleSubmit = (e) => {
-    e.preventDefault();
-    router.push(`/quiz?name=${name}`);
-    console.log('Fazendo uma submissão por meio do react');
+  const handleChange = (e) => {
+    setName(e.target.value);
   };
 
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setName(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/quiz?name=${name}`);
   };
 
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        <title>Star Quiz</title>
+        <title>{db.title}</title>
       </Head>
       <QuizContainer>
         <QuizLogo />
@@ -56,16 +46,18 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={hamndleSubmit}>
-              <input placeholder="nome" onChange={handleChange} />
-              <button type="submit" disabled={name.length === 0}>
-                jogar
-                {' '}
-                {name}
-              </button>
-
+            <p>{db.description}</p>
+            <form onSubmit={handleSubmit}>
+              <Input
+                name="nomeDoUsuario"
+                onChange={handleChange}
+                placeholder="Diz ai seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
             </form>
-
           </Widget.Content>
         </Widget>
 
